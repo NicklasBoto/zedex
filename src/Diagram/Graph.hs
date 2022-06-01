f6f31a7bf1792436c7a739fee62d74aa4fa80974
Nicklas Botö <nicklas.boto@gmail.com>
2022-06-01 02:50:07 +0200

    First attempt at graph represenation datatype

--- src/Diagram/Graph.hs
+++ src/Diagram/Graph.hs
@@ -1,8 +1,27 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
 
 -- | 
 data NodeType
     = Red   Double
     | Green Double 
     | Box
+    | Blank
     deriving (Eq, Show)
+
+data Node = Node 
+    { nodetype :: NodeType
+    , adjacent :: [Node]
+    }
+
+type Graph = [Node]
+
+translate :: Graph -> String
+translate = concatMap (uncurry $ go 0) . zip [0..]
+    where go rnum qnum Node{..} = case nodetype of
+            Red alpha -> ""
+            Green alpha -> ""
+            Box -> ""
+            Blank -> ""
+
 
