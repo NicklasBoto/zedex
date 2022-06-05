8b93ad5479e6f72d6c711481920f7dd4901f7f19
Nicklas Botö <nicklas.boto@gmail.com>
2022-06-05 22:50:04 +0200

    Clean up graph library examples

--- src/Diagram/Graph.hs
+++ src/Diagram/Graph.hs
@@ -1,27 +1,27 @@
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Diagram.Graph where
 
 import Control.Monad.State
 import Diagram.Template ( genAbstractions )
-import qualified Data.Sized as S
 
 data Basis
-    = Z
-    | X
-    | H
-    | W
-    deriving (Eq, Show)
+  = Z
+  | X
+  | H
+  | W
+  deriving (Eq, Show)
 
 data Node = Node
-    { basis :: Maybe Basis
-    , arg   :: Maybe Double
-    , arity :: Int
-    , out   :: [] Node
-    , tag   :: Int
-    } deriving (Eq, Show)
+  { basis :: Maybe Basis
+  , arg   :: Maybe Double
+  , arity :: Int
+  , out   :: [] Node
+  , tag   :: Int
+  } deriving (Eq, Show)
 
 newtype Graph a = Gr { unGr :: State Int a }
     deriving (Functor, Applicative, Monad, MonadState Int)
@@ -31,9 +31,9 @@ runGraph = flip evalState 0 . unGr
 
 node :: Basis -> Double -> Int -> [Node] -> Graph Node
 node b a n o = do
-    t <- get
-    put (t + 1)
-    return $ Node (Just b) (Just a) n o t
+  t <- get
+  put (t + 1)
+  return $ Node (Just b) (Just a) n o t
 {-# INLINE node #-}
 
 abstraction :: Basis -> Double -> ([Node] -> [Node]) -> [Node] -> Graph Node
@@ -50,45 +50,34 @@ $(genAbstractions "xi"     5)
 $(genAbstractions "heta"   5)
 $(genAbstractions "womega" 5)
 
-eigenX :: Double -> Int -> Graph Node
-eigenX alpha n = xi1 alpha (replicate n)
-             =<< node X 0 1 [] 
+wire :: Graph Node
+wire = do
+  t <- get
+  put (t + 1)
+  return $ Node Nothing Nothing 1 [] t
 
-eigenZ :: Double -> Int -> Graph Node
-eigenZ alpha n = zeta1 alpha (replicate n)
-             =<< node Z 0 1 [] 
+eigenGenerator :: Basis -> Double -> Int -> Graph Node
+eigenGenerator basis alpha n = node basis alpha 0 =<< replicateM n wire
 
-had :: Node -> Graph Node
-had = heta1 0 pure
+eigenZ, eigenX, eigenH, eigenW  :: Double -> Int -> Graph Node
+eigenZ = eigenGenerator Z
+eigenX = eigenGenerator X
+eigenH = eigenGenerator H
+eigenW = eigenGenerator W
 
-splits :: Node -> Graph Node
-splits = heta1 0 (\x -> [x,x])
+new :: Int -> Graph Node
+new n = eigenZ (pi * fromIntegral n) 1
 
 splitn :: Int -> Node -> Graph Node
 splitn = heta1 0 . replicate
 
 mergen :: [Node] -> Graph Node
-mergen = heta 0 (pure . head)
+mergen = heta 0 \case
+  []  -> []
+  n:_ -> [n]
 
 pauliX :: Node -> Graph Node
 pauliX = xi1 pi pure
 
-xNode :: Double -> [Node] -> Graph Node
-xNode = flip xi id
-
-isometry :: Double -> Node -> Graph Node
-isometry = flip xi1 (\x -> [x,x]) 
-
-share :: Node -> Graph Node
-share = isometry 0
-
-partIsometry :: Double -> Node -> Node -> Graph Node
-partIsometry = flip xi2 (\x _ -> [x])
-
-fuse :: Node -> Node -> Graph Node
-fuse = partIsometry 0
-
--- ghz :: Graph Node
--- ghz = do
---     a <- xi 0 
---     b <- xi 0 (\x -> x) []
+had :: Node -> Graph Node
+had = heta1 0 pure
